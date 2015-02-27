af6a97cf6669be07b9ba2a480afe6f8fd093debe
Samuel T <samtjok@gmail.com>
2015-02-26 23:10:18 -0600

    Added Hibernate, SQLite DB & Patient Entity.

---
+++ SecretSix/app/model/PatientEntity.java
@@ -0,0 +1,53 @@
+package model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Samuel_Tjokrosoesilo on 2/26/2015.
+ */
+@Entity
+@Table(name = "Patient", schema = "", catalog = "")
+public class PatientEntity {
+    private int id;
+    private String name;
+
+    @Id
+    @Column(name = "Id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "Name", nullable = false, insertable = true, updatable = true, length = 2000000000)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PatientEntity that = (PatientEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
