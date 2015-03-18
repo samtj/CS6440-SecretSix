2f26f40c153e9b8e9c8cbd872b28b81703ff59bb
pardisdesigner <amansoori@gmail.com>
2015-03-17 22:19:37 -0500

    added function to return an object of all unique conditions with each condition having a list ofpatient data and count of patients

--- SecretSix/app/repositories/SsSqLiteHelper.java
+++ SecretSix/app/repositories/SsSqLiteHelper.java
@@ -5,7 +5,7 @@ package repositories;
  */
 public class SsSqLiteHelper {
 
-    public static final String DB_LOCATION = "/SecretSix/db/secretsix.db";
+    public static final String DB_LOCATION = "../SecretSix/db/secretsix.db";
 
     public static final String COLUMN_USERID = "UserId";
     public static final String COLUMN_USERNAME = "UserName";
